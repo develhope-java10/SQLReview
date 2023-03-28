f84ce4079bc47df31edf5071399372abfc4eeafc
Giulia <giuliac35@gmail.com>
2023-03-28 20:24:01 +0200

    last queries of Esercizio1 completed

--- giulia_esercizio1.sql
+++ giulia_esercizio1.sql
@@ -13,8 +13,21 @@
 -- HAVING COUNT(Students.id) >= 2;
 
 -- Scrivere una query per elencare tutti i corsi senza nessun iscritto --
-SELECT Courses.name FROM CourseEnrollments
+--SELECT Courses.name FROM CourseEnrollments
+--INNER JOIN Students ON CourseEnrollments.student_id = Students.id
+--INNER JOIN Courses ON CourseEnrollments.course_id = Courses.id
+--GROUP BY Courses.id
+--HAVING COUNT(Students.id) = 0
+
+--Scrivere una query che elenchi per ogni riga il nome di ogni corso con il corrispondente numero di iscritti
+--SELECT Courses.name, COUNT(Students.id) AS students_enrolled FROM CourseEnrollments
+--INNER JOIN Students ON CourseEnrollments.student_id = Students.id
+--INNER JOIN Courses ON CourseEnrollments.course_id = Courses.id
+--GROUP BY Courses.name;
+
+--Scrivere una query che elenchi per ogni riga il nome ed il cognome di ogni studente
+--con il corrispondente numero di corsi a cui è iscritto/a
+SELECT students.first_name, students.last_name, COUNT(courses.id) AS number_of_courses FROM CourseEnrollments
 INNER JOIN Students ON CourseEnrollments.student_id = Students.id
 INNER JOIN Courses ON CourseEnrollments.course_id = Courses.id
-GROUP BY Courses.id
-HAVING COUNT(Students.id) = 0;
+GROUP BY CourseEnrollments.student_id;
