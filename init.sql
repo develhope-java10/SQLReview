759e526e73db7f6c560e31944be6e09e000ff844
lulanoise <lucrezia.rumore98@gmail.com>
2023-04-06 09:47:43 +0200

    first commit

--- init.sql
+++ init.sql
@@ -5,14 +5,12 @@ CREATE TABLE IF NOT EXISTS Students(
 );
 
 CREATE TABLE IF NOT EXISTS Courses(
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255)
-);
+id INTEGER PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(255));
 
 CREATE TABLE IF NOT EXISTS CourseEnrollments(
     student_id INTEGER, 
-    course_id INTEGER, 
-
+    course_id INTEGER,
     FOREIGN KEY (student_id) REFERENCES Students(id),
     FOREIGN KEY (course_id) REFERENCES Courses(id)
 );
