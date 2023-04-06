ec6dd2db2dd25119b907034e986e9448a2f20d6f
Edduard79 <eduard.22@gmail.com>
2023-04-06 20:05:28 +0200

    updated init file with tutors table

--- init.sql
+++ init.sql
@@ -16,3 +16,17 @@ CREATE TABLE IF NOT EXISTS CourseEnrollments(
     FOREIGN KEY (student_id) REFERENCES Students(id),
     FOREIGN KEY (course_id) REFERENCES Courses(id)
 );
+
+CREATE TABLE IF NOT EXISTS Tutors(
+	tutor_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    exp_years INTEGER,
+    class VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS TutorsAssignement(
+	tutor_id INTEGER,
+    course_id INTEGER,
+    
+    FOREIGN KEY (tutor_id) REFERENCES Tutors(id),
+    FOREIGN KEY (course_id) REFERENCES Courses(id)
+);    
